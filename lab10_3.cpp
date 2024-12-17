2e9c4dbac181a8fa9c4e2b2cc199a98b2acf9a51
LAB303-6 <lab303-6@vdieng.local>
2024-12-17 16:34:27 +0700

    lab10.3.cpp completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main(){
+    ifstream source("score.txt");
+    double num, sum = 0, sumsq = 0;
+    int n = 0;
+    string text;
+    while (getline(source, text)){
+        num = atof(text.c_str());
+        sum += num;
+        sumsq += num *num;
+        n++;
+    }
+    source.close();
+    double mean = sum / n;
+    double STD = sqrt((sumsq / n) - (mean * mean));
+    cout << "Number of data = " << n << endl;
+    cout << setprecision(3);
+    cout << "Mean = " <<  mean << endl;
+    cout << "Standard deviation = " << STD << endl;
+    return 0;
+}
